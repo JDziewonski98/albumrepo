8e0553a9831388238b85f878f5044a48ec030eea
JD908 <46234488+JD908@users.noreply.github.com>
2020-09-01 12:36:22 -0700

    'init'

---
+++ src/main/java/com/imagerepo/albumrepo/AlbumrepoApplication.java
@@ -0,0 +1,13 @@
+package com.imagerepo.albumrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imagerepo/albumrepo/AlbumrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagerepo.albumrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
