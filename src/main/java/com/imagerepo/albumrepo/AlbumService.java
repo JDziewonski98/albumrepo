931844cea74858365a566d149c22cfefc64ea632
JD908 <46234488+JD908@users.noreply.github.com>
2020-09-03 11:46:55 -0700

    got rid of comments

--- src/main/java/com/imagerepo/albumrepo/AlbumService.java
+++ src/main/java/com/imagerepo/albumrepo/AlbumService.java
@@ -5,12 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.*;
-import java.util.zip.DataFormatException;
-import java.util.zip.Deflater;
-import java.util.zip.Inflater;
 
 @Service
 public class AlbumService {
@@ -50,52 +46,4 @@ public class AlbumService {
         albums.removeIf(a -> !(Arrays.equals(a.getGenres().toArray(), genres)));
         return albums;
     }
-
-//    private byte[] compressBytes(byte[] data) {
-//        Deflater deflater = new Deflater();
-//        deflater.setInput(data);
-//        deflater.finish();
-//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-//        byte[] buffer = new byte[1024];
-//        while (!deflater.finished()) {
-//            int count = deflater.deflate(buffer);
-//            outputStream.write(buffer, 0, count);
-//        }
-//        try {
-//            outputStream.close();
-//        } catch (IOException e) {
-//        }
-//        System.out.println("Compressed Image Byte Size - " + outputStream.toByteArray().length);
-//        return outputStream.toByteArray();
-//    }
-//
-//    private byte[] decompress(byte[] data) {
-//        Inflater inflater = new Inflater();
-//        inflater.setInput(data);
-//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-//        byte[] buffer = new byte[1024];
-//        try {
-//            while (!inflater.finished()) {
-//                int cnt = inflater.inflate(buffer);
-//                outputStream.write(buffer, 0 , cnt);
-//            }
-//            outputStream.close();
-//        }
-//        catch (DataFormatException | IOException e) {
-//            //todo handle
-//        }
-//        return outputStream.toByteArray();
-//    }
-//
-//    private HashSet<AlbumModel> convertToImages(HashSet<AlbumModel> encryptedAlbums) {
-//        HashSet<AlbumModel> decryptedAlbums = new HashSet<>(Collections.emptySet());
-//        for (AlbumModel album : encryptedAlbums) {
-//            decryptedAlbums.add(
-//                    new AlbumModel(album.getFilename(),album.getTitle(),
-//                            album.getDescription(), decompress(album.getPicture()), album.getGenres(),
-//                            album.getType(), album.getArtist())
-//            );
-//        }
-//        return decryptedAlbums;
-//    }
 }
